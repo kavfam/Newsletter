782eec5772102ced82663fb9a6c553729ccacace
Patrick Kavanagh <kreollc@hotmail.com>
2022-02-17 20:19:56 -0500

    clean app.js mods to .gitignore

--- app.js
+++ app.js
@@ -4,47 +4,12 @@
 
 const cl = (...args) => console.log(...args);
 
-//const configData = require("./config.json");
-//console.log(typeof configData);
-//console.log(configData);
-// const audienceId = configData.audienceId;
-// const apiString = configData.apiString;
-// const server = configData.server;
-// const apiAuth = "myname:" + apiString;
-
-//const myRound = (num) => Math.round(num * 100, 2) / 100;
-// const configData = require("./config.json");
-// //console.log(configData);
-// let audienceId = configData.audienceId;
-// let apiString = configData.apiString;
-// let server = configData.server;
-// let apiAuth = "myname:" + apiString;
-// cl(audienceId);
-// cl(apiString);
-// cl(server);
-// cl(apiAuth);
-
-// for heroku environment vars ..
-// const aws = require("aws-sdk");
-// let n1 = new aws.N1({
-//   audienceId: process.env.N1_KEY,
-//   apiString: process.env.N1_SECRET,
-//   server: process.env.N1_SERVER,
-// });
-// cl(n1.audienceId);
-// cl(n1.apiString);
-// cl(n1.server);
-
 // use dotenv to access your .env file
 require("dotenv").config();
 const audienceId = process.env.N1_KEY;
 const apiString = process.env.N1_SECRET;
 const server = process.env.N1_SERVER;
 const apiAuth = "myname:" + apiString;
-// cl(audienceId);
-// cl(apiString);
-// cl(server);
-// cl("------------------");
 
 // note: __dirname did not seem to work in Edge browser so set to variable name and that worked (in GET method)
 const dirName = __dirname;
@@ -69,11 +34,6 @@ app.get("/", function (req, res) {
   //cl(dirName + "/index.html");
   res.sendFile(__dirname + "/signup.html");
 });
-
-// set up variables for POST: audienceID, apiAuth and server
-// const audienceId = "3e151642f9"; // from your account on mailchimp
-// const apiAuth = "anyname:" + "7beed9cdbfe84283feb55c244690f793-us20"; // API key from your account on mailchimp
-// const server = "us20"; // this is at the end of your API key
 
 //post
 app.post("/", function (req, res) {
